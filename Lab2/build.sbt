b3dbdc3b46deb0d23ad92e9a7d8c1ca98db998ea
Antoś Jurevič <yurevich-anton@mail.ru>
2019-03-10 19:04:55 +0300

    Update lab2

--- Lab2/build.sbt
+++ Lab2/build.sbt
@@ -3,3 +3,9 @@ name := "Lab2"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+libraryDependencies  ++= Seq(
+  "org.scalanlp" %% "breeze" % "0.13.2",
+  "org.scalanlp" %% "breeze-natives" % "0.13.2",
+  "org.scalanlp" %% "breeze-viz" % "0.13.2"
+)
---
+++ Lab2/src/main/scala/Lab2.scala
@@ -0,0 +1,59 @@
+import breeze.linalg.DenseVector
+import breeze.stats._
+
+import scala.io.{BufferedSource, Source}
+
+object Lab2 {
+
+  /**
+    * @return надёжность оценки
+    */
+  def getReliability: Double = {
+    0.95
+  }
+
+  /**
+    * @return имя dataset - файла
+    */
+  def getFilename: String = {
+    "dataset.txt"
+  }
+
+  /**
+    * Метод парсинга датасета
+    * @return mpg - DenseVector с данными
+    */
+  def parseFile(filename: String): (DenseVector[Double], DenseVector[Double]) = {
+
+    val mpg = DenseVector.zeros[Double](392)
+    val horsepower = DenseVector.zeros[Double](392)
+
+    val file: BufferedSource = Source.fromFile(filename)
+    val lines = file.getLines().toList
+    var i = 0
+
+    for (line <- lines) {
+      val temp = line.split(" ")
+      if (temp(3) != "?") {
+        mpg(i) = temp(0).toDouble
+        horsepower(i) = temp(3).toDouble
+        i+=1
+      }
+    }
+    (mpg, horsepower)
+  }
+
+  def main (args: Array[String]): Unit = {
+    val (mpg, horsepower) = parseFile(getFilename)  //данные
+
+    /*дисперсия*/
+    val mpgDespersion        = variance(mpg)
+    val horsepowerDespersion = variance(horsepower)
+    /*мат.ожидание*/
+    val mpgAverage           = mean(mpg)
+    val horsepowerAverage    = mean(horsepower)
+
+
+  }
+
+}
